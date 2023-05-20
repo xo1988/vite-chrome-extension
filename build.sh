0fb6f5f3bb1821ff908a98068885e692d8e7ab4a
xo <947936711@qq.com>
2023-05-20 22:48:42 +0800

    简化为两个配置文件

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 mkdir dist
 rm -rf dist/* &&\
 
+vite build -c vite.config.script.js &&\
 vite build -c vite.config.js &&\
-vite build -c vite.config.background.js &&\
-vite build -c vite.config.content_scripts.js &&\
 
 cp -rf chrome/* dist
 
