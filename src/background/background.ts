eed91a048f0e97a456883aa87e4273db9f04164e
xo <947936711@qq.com>
2023-06-08 21:51:17 +0800

    解决chrome启动background.js inactive问题

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,8 @@
 console.log("hello background")
+
+// when chrome start, the service work is inactive
+// then add below listener to keep service work active
+chrome.runtime.onStartup.addListener( () => {
+    console.log(`onStartup()`);
+});
+
